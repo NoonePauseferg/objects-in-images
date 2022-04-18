70f9e684c2e1ab8b627ed93cd8d19378ed0fcd70
Вадим Воробьев <vadimvorobev@Vadims-MacBook-Pro.local>
2022-04-18 15:24:57 +0300

    ~

--- test.py
+++ test.py
@@ -8,6 +8,7 @@ class Test(unittest.TestCase):
         super().__init__(*a, **kw)
 
     def test_generalCase(self):
+        """based on PCA algorithm"""
         ball_vector = generalCase(self.data_ball[:4])
         kube_vector = generalCase(self.data_kube[4:])
         self.assertEqual(comparison(kube_vector.reshape((1, -1)), ball_vector.reshape((1, -1)),
@@ -16,6 +17,7 @@ class Test(unittest.TestCase):
                     self.data_ball[0]), "круг")
 
     def test_Morphological(self):
+        """based on morphological method"""
         self.assertEqual(plausibility(self.data_kube[0], self.data_ball[0]), False)
         self.assertEqual(plausibility(self.data_kube[0], self.data_kube[1]), True)
         self.assertEqual(plausibility(self.data_kube[2], self.data_kube[3]), True)
